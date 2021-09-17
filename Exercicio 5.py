f802d519fbc264dd145d352aaafd02fd4898a7ec
Ana Karla Santana <82245801+anakarlasantana@users.noreply.github.com>
2021-09-16 21:07:14 -0300

    Add files via upload

---
+++ Exercicio 5.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Nessa aula, vamos aprender quais são os operadores aritméticos do Python 
+e também sua ordem de precedência dentro de expressões matemáticas. Veja 
+como funcionam os operadores de adição, subtração, multiplicação, divisão, 
+exponenciação e quociente na linguagem Python.
+
+@author: santana
+"""
+
